45d6e367d36b4ae078c3cf860cc53b20a3b5a499
kaymax239 <TUEMAIL@gmail.com>
2026-06-18 09:41:46 -0600

    Add SMCP Trainer story and mission 01 base

---
+++ data/story.ts
@@ -0,0 +1,21 @@
+export const vessel = {
+  name: "MV Ocean Pioneer",
+};
+
+export const characters = {
+  captain: {
+    name: "Captain David Jones",
+  },
+
+  bosun: {
+    name: "Robert Hayes",
+  },
+
+  cadet: {
+    name: "Officer Cadet",
+  },
+};
+
+export const academy = {
+  name: "Maritime Academy",
+};
